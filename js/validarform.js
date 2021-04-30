e623f4f4eee260e3097a82c24e56e79b732f3ef3
andreinacat <andrea.verdugmunoz@gmail.com>
2021-04-30 04:08:50 -0400

    validar_nombre

---
+++ js/validarform.js
@@ -0,0 +1,24 @@
+function validar_form(){
+    
+}
+
+function validar_nombre(){
+    var name = document.getElementById('txtnombre').value;
+    if (name.length < 3){
+        alert('el nombre debe tener al menos 3 caracteres');
+        return false;
+    } 
+
+
+}
+function validar_apellido(){
+
+}
+function validar_email(){
+
+
+}
+function validar_comentario(){
+
+}
+
